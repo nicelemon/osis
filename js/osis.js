5055606e4b1b8eb6f2b66c8cfb5ae8f962715ba6
nicelemon <zackry.lerner@gmail.com>
2015-03-14 18:08:42 -0400

    Moving/disabling music js stuff, and implementing parallax shadow stuff

--- js/osis.js
+++ js/osis.js
@@ -1,10 +1,14 @@
 //// Dev Stuff - stuff to removed eventually
 
-// Random bg colors
+// Random bg/border colors
 $(function() {
     $(".bg-fresh").each(function() {
         var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
          $(this).css("background-color", hue);
+    });
+    $(".border-fresh").each(function() {
+        var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+         $(this).css("border-color", hue);
     });
 });
 
@@ -12,76 +16,35 @@ $(function() {
 //// Forever stuff - probably shouldn't remove
 
 
-// Seamless Audio Loop stuff
-var loop = new SeamlessLoop();
-
-//check if the browser can play MP3's. If not, use ogg.
-var audio  = document.createElement("audio"),
-canPlayMP3 = (typeof audio.canPlayType === "function" &&
-              audio.canPlayType("audio/mpeg") !== "");
-if (canPlayMP3===true) {
-  loop.addUri("/music/amen_break_0.mp3", 2810, "secondPage");
-  // loop.addUri("/music/amen_break_0.mp3", 2810, "sound2");
-  loop.addUri("/music/funky_drummer_0.mp3", 8750, "3rdPage");
-  // loop.addUri("/music/funky_drummer_0.mp3", 8750, "sound2");
-  loop.addUri("", 0, "firstPage");
-  // loop.addUri("", 0, "sound2");
-} else {
-  loop.addUri("/music/amen_break_0.ogg", 2810, "sound1");
-  // loop.addUri("/music/amen_break_0.ogg", 2810, "sound2");
-  loop.addUri("/music/funky_drummer_0.ogg", 8750, "sound1");
-  // loop.addUri("/music/funky_drummer_0.ogg", 8750, "sound2");
-  loop.addUri("", 0, "sound1");
-  // loop.addUri("", 0, "sound2");
-}
-
-var trackNumber = 1;
-
-function soundsLoadedAmen() {
-  loop.start("firstPage");
-  // n++;
-  // loopAmen.update("sound" + n, false);
-};
-
-loop.callback(soundsLoadedAmen);
-
-// // Toggle loop
-// function toggle(button) {
-//   if (button.value == "OFF") {
-//     button.value = "ON";
-//     loop.stop();
-//   } else {
-//     button.value = "OFF";
-//     loop.stop();
-//     loop.start("sound" + trackNumber);
-//   }
-// }
-
-// Toggle volume
-function toggle(button) {
-  if (button.value == "OFF") {
-    button.value = "ON";
-    loop.volume(0);
-  } else {
-    button.value = "OFF";
-    loop.volume(1);
-  }
-}
-
-// function soundsLoadedFunky() {
-//   var n = 1;
-//   loopFunky.start("sound" + n);
-//   n++;
-//   loopFunky.update("sound" + n, false);
-// };
-
-// loopFunky.callback(soundsLoadedFunky);
-
-
-// Hashchange testing
-
-
 $(document).ready(function() {
+
+// Shadow postitioning
+	$( ".section" ).mousemove(function( event ) {
+		var el = $(this),
+			strength = 80,
+			h = el.outerHeight(),
+			w = el.outerWidth(),
+			sh = strength / h,
+			sw = strength / w,
+			pageX = event.pageX || event.clientX,
+			pageY = event.pageY || event.clientY,
+			pageX = (pageX - el.offset().left) - (w / 2),
+			pageY = (pageY - el.offset().top) - (h / 2),
+			newX = Math.round(((sw * pageX)) * - 1),
+			newY = Math.round(((sh * pageY)) * - 1),
+			negX = (newX / 5) * - 1,
+			negY = (newY / 5) * - 1;
+		$(".shady").css({
+			"-webkit-box-shadow: 21px 23px 55px -8px rgba(0,0,0,0.55);",
+			"top": negY,
+			"left": negX
+	    });
+		$(".outlined").css({
+			"webkit-filter": "drop-shadow(" + newX + "px " + newY + "px 5px rgba(0,0,0,0.3))",
+			"top": negY,
+			"left": negX
+		});
+	});
 
 // Initialize scroll snapping stuff
 	$('#fullpage').fullpage({
@@ -171,12 +134,4 @@ $(document).ready(function() {
 
 	});
 
-	$(window).on('hashchange', function () {
-		var hash = location.hash.replace( /^#/, '' );
-		document.getElementById("consoleHashNumber").innerHTML =  hash;
-	    var trackNumber = 2;
-	    loop.stop();
-		loop.start(hash);
-	});
-
-});	
+});
