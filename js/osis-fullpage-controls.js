a7fce032ccdb9a259b2a466f9db398196f11d26d
nicelemon <zackry.lerner@gmail.com>
2015-03-23 19:45:08 -0400

    Adding background video to ratcrawl and misc. cleanup

--- js/osis-fullpage-controls.js
+++ js/osis-fullpage-controls.js
@@ -4,17 +4,30 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', 'lastPage'],
 		menu: '#menu',
-    css3: true,
-    scrollingSpeed: 700,
-    autoScrolling: true,
-    fitToSection: true,
-    scrollBar: true,
-    easing: 'easeInOutCubic',
-    easingcss3: 'ease',
-    loopHorizontal: true,
-    scrollOverflow: false,
-    touchSensitivity: 15,
-    normalScrollElementTouchThreshold: 5,
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        scrollBar: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopHorizontal: true,
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        onLeave: function(index, nextIndex, direction){
+            var vid = document.getElementById("dirtyGround");
+            if (index == 1 && direction == "down") {
+                vid.play();
+            }
+            else if (index == 2 && direction == "up" || direction == "down") {
+                vid.pause();
+            }
+            else if (index == 3 && direction == "up") {
+                vid.play();
+            };
+        }
 
 	});
 
