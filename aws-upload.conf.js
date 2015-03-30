d82e9c5e3b6a5445e33d207b9cf3309a27638758
nicelemon <zackry.lerner@gmail.com>
2015-03-30 17:39:54 -0400

    Adding upload config

---
+++ aws-upload.conf.js
@@ -0,0 +1,10 @@
+module.exports = {
+    credentials:"aws-credentials.json",
+    bucketName:"staging.osis.tv",
+    patterns:[
+        "js/*.js",
+        "css/*.css",
+        "img/*",
+        "index.html"
+    ]
+}
